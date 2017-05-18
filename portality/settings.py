fe4849ef90654d06c226c97748ec48269ed975e1
KJ Kim <genonfire@gmail.com>
2017-05-19 01:23:21 +0900

    disable DEBUG

--- portality/settings.py
+++ portality/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 if (os.environ.has_key('DJANGO_DEBUG')):
     if (os.environ['DJANGO_DEBUG'] == 'Debug'):
         DEBUG = True
