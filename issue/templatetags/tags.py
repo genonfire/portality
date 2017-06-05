6f3e90f6d9cbd8dd4a5aa8069bdde5c3ef40c370
KJ Kim <genonfire@gmail.com>
2017-06-05 21:29:45 +0900

    fix best counter bug

--- issue/templatetags/tags.py
+++ issue/templatetags/tags.py
@@ -1,7 +1,10 @@
 from django import template
+from django.conf import settings
+
 register = template.Library()
 
 @register.simple_tag
 def getIndex(counter, index):
-    new_index = (int(index) - 1) * 10 + counter
+    acom = settings.BEST_LIST_LIMIT / 10
+    new_index = (int(index) * acom - acom) * 10 + counter
     return new_index
